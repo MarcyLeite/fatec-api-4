bbd1030049b49579c98d31486433b5e769d68147
Matheus Marciano Leite <mm.leitea@outlook.com>
2025-04-27 16:27:44 +0000

    chore: Atualizar gitmodules e dev script

--- scripts/dev-up.sh
+++ scripts/dev-up.sh
@@ -2,4 +2,4 @@ export DUID=$(id -u)
 export DGID=$(id -g)
 
 mkdir ~/.gradle 
-docker compose -f compose-dev.yaml up -d --build --remove-orphans
+docker compose -f compose-dev.yaml up -d --build --remove-orphans --no-deps
